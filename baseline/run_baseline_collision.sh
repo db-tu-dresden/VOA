56f546ff555141a88d023018f4f563058a539df0
Lennart Schmidt <lennart.schmidt1@tu-dresden>
2024-03-05 09:46:29 +0100

    Reworked baseline run scripts
    added new scripts that allow to command line parameters. This makes it possible to more easily nest them.
    removed old scripts, since they are now obsolete

---
+++ baseline/run_baseline_collision.sh
@@ -0,0 +1,46 @@
+benchmark="baseline_collision"  # benchmark name
+repeats=7                       # test repeats
+node=0                          # memory node
+probe=4                         # probe amount in gib
+hash_table_size=1024
+shift_size=9                
+thread_count=1
+file_override=0
+file_override_name=""
+while getopts h:p:m:t:s:r:f: flag
+do 
+    case "${flag}" in
+        h) hash_table_size=${OPTARG};;
+        p) probe=${OPTARG};;
+        m) node=${OPTARG};;
+        t) thread_count=${OPTARG};;
+        s) shift_size=${OPTARG};;
+        r) repeats=${OPTARG};;
+        f) file_override=1 
+           file_override_name=${OPTARG};;
+    esac
+done
+
+memory="RAM"
+if ((node >= 8))
+then 
+    memory="HBM"
+fi
+
+datetime=`date +"%Y-%m-%d %T"`
+datetime=${datetime//[\ ]/_}
+datetime=${datetime//[:]/-}
+
+filename="$benchmark"_"$datetime"
+
+if [ file_override > 0 ]
+then
+    filename="$file_override_name"
+fi
+
+filename="$filename"_"$memory"
+
+for i in {1..8}
+do
+    ./run_benchmark.sh -m $node -p $probe -t $thread_count -c i -s $shift_size -r $repeats -f $filename -h $hash_table_size;
+done
